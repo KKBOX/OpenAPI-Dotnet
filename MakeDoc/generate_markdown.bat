85feb2486b2407251773f08adbf0819b1dddce6f
Pou Lin <poumason@live.com>
2018-01-29 14:09:07 +0800

    fixed .net standard complie error

--- MakeDoc/generate_markdown.bat
+++ MakeDoc/generate_markdown.bat
@@ -1,7 +1,7 @@
 cd ..
 cd ..
 cd MakeDoc
-MarkdownGenerator.exe KKBOX.OpenAPI.Universal.dll
+MarkdownGenerator.exe KKBOX.OpenAPI.Standard.dll
 IF EXIST ".\md\Home.md" copy ".\md\Home.md" "..\Doc"
 IF EXIST ".\md\KKBOX.OpenAPI.md" copy ".\md\KKBOX.OpenAPI.md" "..\Doc"
 IF EXIST ".\md\KKBOX.OpenAPI.ServiceModel.md" copy ".\md\KKBOX.OpenAPI.ServiceModel.md" "..\Doc"
